2bd172c82110555cb3a07f9c4942d9eac441aa50
Fabien JUIF <fabien.juif@zenika.com>
2017-10-27 12:39:27 +0200

    :white_check_mark: getAction

--- src/getAction.spec.js
+++ src/getAction.spec.js
@@ -6,7 +6,33 @@ describe('getAction maper', () => {
     const call = {
       name: 'actionName',
       type: 'action', // simple mobx-state-tree action
-      context: {}, // FIXME : HELP ME HOW TO THIS IS WELL ?
+      context: { $treenode: { path: '/this/is/a/path' } },
+      args: ['first', { second: 'argument' }],
+    }
+
+    const res = getAction(call)
+
+    expect(res).toMatchSnapshot()
+  })
+
+  it('should map an undefined type', () => {
+    const call = {
+      name: 'actionName',
+      type: 'undefined type',
+      context: { $treenode: { path: '/this/is/a/path' } },
+      args: ['first', { second: 'argument' }],
+    }
+
+    const res = getAction(call)
+
+    expect(res).toMatchSnapshot()
+  })
+
+  it('should map an asynchronous action that ends', () => {
+    const call = {
+      name: 'actionName',
+      type: 'process_return', // asynchronous that ends action
+      context: { $treenode: { path: '/this/is/a/path' } },
       args: ['first', { second: 'argument' }],
     }
 
