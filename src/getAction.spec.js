96cecdee5308abf240d1f272beb3eed3ab602b76
Fabien JUIF <fabien.juif@zenika.com>
2017-10-27 16:57:05 +0200

    :bug: fix async action (mobx-state-tree v1.1)

--- src/getAction.spec.js
+++ src/getAction.spec.js
@@ -28,10 +28,24 @@ describe('getAction maper', () => {
     expect(res).toMatchSnapshot()
   })
 
-  it('should map an asynchronous action that ends', () => {
+  it('should map an asynchronous action that ends [mst<=1.0.0]', () => {
     const call = {
       name: 'actionName',
-      type: 'process_return', // asynchronous that ends action
+      type: 'process_return', // MST version <= 1.0.0
+      context: { $treenode: { path: '/this/is/a/path' } },
+      args: ['first', { second: 'argument' }],
+    }
+
+    const res = getAction(call)
+
+    expect(res).toMatchSnapshot()
+  })
+
+
+  it('should map an asynchronous action that ends [mst>1.0.0]', () => {
+    const call = {
+      name: 'actionName',
+      type: 'flow_return', // MST version > 1.0.0
       context: { $treenode: { path: '/this/is/a/path' } },
       args: ['first', { second: 'argument' }],
     }
